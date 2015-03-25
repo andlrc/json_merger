1857723f13489c5b771ed119afcc24af0773d253
Andreas AL <andreas@louv.dk>
2015-03-25 12:20:06 +0100

    indicator root is depricated and the value indicator is taking over, this works for every level in the json tree

--- indicators.js
+++ indicators.js
@@ -2,14 +2,14 @@
 var indicators = {
 	// root indicators
 	EXTENDS: '@extends',
-	ROOT: '@root',
 	// used for array, append to end, prepend to beginning, or insert at index:
 	APPEND: '@append',
 	PREPEND: '@prepend',
 	INSERT: '@insert',
 	// @move is the same as @insert but used with @match
 	MOVE: '@move',
-	// rest
+	// Remap value og the object to whats in @value (usefull for appending / prepending to arrays)
+	VALUE: '@value',
 	OVERRIDE: '@override',
 	COMMENT: '@comment',
 	DELETE: '@delete',
